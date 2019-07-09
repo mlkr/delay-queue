e038daed2c69a6046c68f7a6821f2e51552bb682
mlkr <gatsby.yun@gmail.com>
2019-07-09 16:50:27 +0800

    进程自动重启脚本

---
+++ restart.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+while :
+do
+  COUNT=`ps -ef | grep delay-queue |wc -l`
+  if [ "$COUNT" -gt 1 ];
+  then
+    echo "server service is ok"
+  else
+    echo "server service is not exist"
+    nohup delay-queue -c delay-queue.conf > logFile.log 2>&1 &
+  fi
+  sleep 60
+done
